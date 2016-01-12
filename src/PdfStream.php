0b7f51cad197b1455b75f66f88c1f5600ff2cee3
Teis Nielsen <teis.nielsen@viasat.dk>
2016-01-12 21:53:36 +0100

    Fixed restoring

--- src/PdfStream.php
+++ src/PdfStream.php
@@ -4,24 +4,45 @@ namespace TeisVKN\PhpPdfStream;
 
 class PDFStream
 {
-	private $pdfStreamDirectory;
+	const MODE_EXTRACT = 'MODE_EXTRACT';
+	const MODE_RESTORE = 'MODE_RESTORE';
+
+	/**
+	 * @var resource
+	 */
 	private $pdfStreamResource;
 
 	/**
+	 * @var string
+	 */
+	private $pdfStreamDirectory;
+
+	/**
+	 * @var bool
+	 */
+	private $compressStreams;
+	
+	/**
 	 * PdfStream constructor.
 	 * @param $pdfStreamDirectory
+	 * @param bool $compressStreams
+	 * @param bool $compressDstResource
 	 */
-	public function __construct($pdfStreamDirectory) {
+	public function __construct($pdfStreamDirectory, $compressStreams = true, $compressDstResource = true) {
 		$this->pdfStreamDirectory = $pdfStreamDirectory;
+		$this->compressStreams = $compressStreams;
+		$this->compressDstResource = $compressDstResource;
 	}
 
 
 	/**
+	 * Extracts the pdf-streams from the srcResource, to the streamsDirectory,
+	 * and writes the remaining PDF content to $dstResource
 	 * @param resource $srcResource
 	 * @param resource $dstResource
 	 * @throws Exception
 	 */
-	public function extractStreams($srcResource, $dstResource) {
+	public function extract($srcResource, $dstResource) {
 		if (!$this->isValidResource($srcResource)) {
 			throw new Exception('$srcResource is not a valid stream resource');
 		}
@@ -30,12 +51,44 @@ class PDFStream
 			throw new Exception('$dstResource is not a valid stream resource');
 		}
 
+		// write the md5sum of the original file, on first line
+		$this->writeSrcResourceMd5Sum($srcResource, $dstResource);
+
 		while (($buffer = fgets($srcResource)) !== false) {
 			$this->readPdfStreamBegin($dstResource, $buffer) ||
 			$this->readPdfStreamEnd($dstResource, $buffer) ||
 			$this->readPdfStream($buffer) ||
 			$this->readPdfContent($dstResource, $buffer);
 		}
+	}
+
+
+	/**
+	 * Restores the stripped pdf from srcResource, to dstResource by gluing back the extracted
+	 * pdf-streams.
+	 * @param $srcResource
+	 * @param $dstResource
+	 * @throws Exception
+	 */
+	public function restore($srcResource, $dstResource) {
+		if (!$this->isValidResource($srcResource)) {
+			throw new Exception('$srcResource is not a valid stream resource');
+		}
+
+		if (!$this->isValidResource($dstResource)) {
+			throw new Exception('$dstResource is not a valid stream resource');
+		}
+
+		$originalMd5 = $this->readOriginalMd5Sum($srcResource);
+
+		while (($buffer = fgets($srcResource)) !== false) {
+			$this->readPdfStreamBegin($dstResource, $buffer) ||
+			$this->readPdfStreamEnd($dstResource, $buffer, self::MODE_RESTORE) ||
+			$this->readPdfStream($buffer) ||
+			$this->readPdfContent($dstResource, $buffer);
+		}
+
+		$this->validateRestored($dstResource, $originalMd5);
 	}
 
 
@@ -75,17 +128,30 @@ class PDFStream
 	 * @param $buffer
 	 * @return bool
 	 */
-	private function readPdfStreamEnd($dstResource, $buffer) {
+	private function readPdfStreamEnd($dstResource, $buffer, $mode = self::MODE_EXTRACT) {
 		// a pdfStreamResource is currently open (is being read),
 		// and we have now reached the end of it
 		if (is_resource($this->pdfStreamResource) && preg_match('/^endstream\s$/', $buffer)) {
 
-			// write the extracted pdf-stream-content to a file, and get the filename
-			$fileName = $this->writePdfStreamToStreamDirectory();
+			switch ($mode) {
+				case self::MODE_EXTRACT:
+					// $this->pdfStreamResource contains the extracted pdf-stream.
+					// Write the extracted pdf-stream-content to a file, and get the filename
+					$fileName = $this->writePdfStreamToStreamDirectory();
+					// write the fileName of the extracted pdf-stream to the dstResource
+					fwrite($dstResource, $fileName.PHP_EOL);
+					break;
+
+				case self::MODE_RESTORE:
+					// $this->pdfStreamResource contains the file reference of extracted pdf-stream.
+					// Write the content of extracted pdf-stream back to $dstResource
+					$this->restorePdfStream($dstResource);
+					break;
+			}
+
+
 			fclose($this->pdfStreamResource);
 
-			// write the fileName of the extracted pdf-stream to the dstResource
-			fwrite($dstResource, $fileName.PHP_EOL);
 
 			// write the endstream line
 			fwrite($dstResource, $buffer);
@@ -118,6 +184,8 @@ class PDFStream
 		return true;
 	}
 
+
+
 	/**
 	 * @return string filename, being the md5sum of the pdf-stream
 	 * @throws Exception
@@ -127,13 +195,77 @@ class PDFStream
 		$pdfStreamMd5 = md5(stream_get_contents($this->pdfStreamResource));
 
 		rewind($this->pdfStreamResource);
-		$pdfStreamFileName = sprintf('%s/%s',$this->pdfStreamDirectory, $pdfStreamMd5);
+		$fileNamePattern = $this->compressStreams ? '%s/%s.gz' : '%s/%s';
+		$pdfStreamFileName = sprintf($fileNamePattern, $this->pdfStreamDirectory, $pdfStreamMd5);
 
-		if (file_put_contents($pdfStreamFileName, $this->pdfStreamResource) === false) {
+		$result = file_put_contents(
+			$pdfStreamFileName,
+			$this->compressStreams ? gzencode(stream_get_contents($this->pdfStreamResource), 9) : $this->pdfStreamResource
+		);
+		if ($result === false) {
 			throw new Exception(sprintf('Error occurred when writing pdfStream to %s', $pdfStreamFileName));
 		}
 
 		return $pdfStreamMd5;
+	}
+
+	/**
+	 * Calculate md5 of srcResource and write it to dstResource
+	 * @param $srcResource
+	 * @param $dstResource
+	 */
+	private function writeSrcResourceMd5Sum($srcResource, $dstResource) {
+		fwrite(
+			$dstResource,
+			'originalMd5:' . md5(stream_get_contents($srcResource)) . PHP_EOL
+		);
+		rewind($srcResource);
+	}
+
+	/**
+	 * @param $srcResource
+	 * @return string
+	 * @throws Exception
+	 */
+	private function readOriginalMd5Sum($srcResource) {
+		$buffer = fgets($srcResource);
+
+		if (!preg_match('/^originalMd5:(?<md5sum>[a-f0-9]{32})$/', $buffer, $match)) {
+			throw new Exception('Can\'t read originalMd5 sum from $srcResource');
+		}
+
+		return $match['md5sum'];
+	}
+
+	/**
+	 * @param $dstResource
+	 */
+	private function restorePdfStream($dstResource) {
+		rewind($this->pdfStreamResource);
+		$pdfStreamFileName = trim(stream_get_contents($this->pdfStreamResource));
+
+		$fileNamePattern = $this->compressStreams ? '%s/%s.gz' : '%s/%s';
+		$pdfStreamContent = file_get_contents(sprintf($fileNamePattern, $this->pdfStreamDirectory, $pdfStreamFileName));
+
+
+		fwrite(
+			$dstResource,
+			$this->compressStreams ? gzdecode($pdfStreamContent) : $pdfStreamContent
+		);
+
+	}
+
+	/**
+	 * @param $dstResource
+	 * @param $originalMd5
+	 * @throws Exception
+	 */
+	private function validateRestored($dstResource, $originalMd5) {
+		rewind($dstResource);
+
+		if (md5(stream_get_contents($dstResource)) !== $originalMd5) {
+			throw new Exception('MD5 sum of restored file does not match original');
+		}
 	}
 
 
--- tests/PDFStreamTest.php
+++ tests/PDFStreamTest.php
@@ -2,18 +2,46 @@
 
 class PDFStreamTest extends PHPUnit_Framework_TestCase
 {
-	public function testCanParseStream() {
-		$pdfStreamsDirectory = __DIR__.'/data/output/pdfstreams';
+	protected $pdfStream;
+	protected $pdfStreamsDirectory;
 
-		if (!is_dir($pdfStreamsDirectory )) {
-			mkdir($pdfStreamsDirectory );
+	/**
+	 * PDFStreamTest constructor.
+	 */
+	public function __construct() {
+		$this->pdfStreamsDirectory = __DIR__.'/data/output/pdfstreams';
+
+		if (!is_dir($this->pdfStreamsDirectory )) {
+			mkdir($this->pdfStreamsDirectory );
 		}
 
-		$pdfStream = new TeisVKN\PhpPdfStream\PDFStream($pdfStreamsDirectory);
-
-		$pdfStream->extractStreams(
-			fopen(__DIR__.'/data/test.pdf', 'r'),
-			fopen(__DIR__.'/data/output/test.pdf.stripped', 'w')
-		);
+		$this->pdfStream = new TeisVKN\PhpPdfStream\PDFStream($this->pdfStreamsDirectory);;
 	}
+
+
+	public function testCanExtract() {
+		// extract pdf-streams of data/test.pdf to data/output/pdfstreams/ directory,
+		// and write the remaining parts of the pdf to data/output/test.pdf.stripped
+		$this->pdfStream->extract(
+			$srcResource = fopen(__DIR__.'/data/test.pdf', 'r'),
+			$dstResource = fopen(__DIR__.'/data/output/test.pdf.stripped', 'w')
+		);
+
+		fclose($srcResource);
+		fclose($dstResource);
+	}
+
+	/** @depends testCanExtract */
+	public function testCanRestore() {
+		// read the data/output/test.pdf.stripped, and restore it to data/output/test.restored.pdf
+		// by gluing back in the extracted pdf-streams from data/output/pdfstreams/ directory.
+		$this->pdfStream->restore(
+			$srcResource = fopen(__DIR__.'/data/output/test.pdf.stripped', 'r'),
+			$dstResource = fopen(__DIR__.'/data/output/test.restored.pdf', 'w+') // must be w+ to enable md5 validation
+		);
+
+		fclose($srcResource);
+		fclose($dstResource);
+	}
+
 }
